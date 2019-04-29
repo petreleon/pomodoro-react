51b85892685c8ddafa4671e4ed2f0ec1d10f0fa3
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-04-29 22:14:55 +0300

    changes

---
+++ src/pomodoroHome.js
@@ -0,0 +1,12 @@
+import {Card, CardBody} from 'reactstrap';
+import * as React from "react";
+
+class pomodoroHome extends React.Component {
+    render(){
+        return <CardBody className='w-100'>
+            Pomodoro
+        </CardBody>
+    }
+}
+
+export default pomodoroHome;
---
+++ src/pomodoroSettings.js
@@ -0,0 +1,12 @@
+import {Card, CardBody} from 'reactstrap';
+import * as React from "react";
+
+class pomodoroSettings extends React.Component {
+    render(){
+        return <div className='w-100'>
+            Pomodoro
+        </div>
+    }
+}
+
+export default pomodoroSettings;
